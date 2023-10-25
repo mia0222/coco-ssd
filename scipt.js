8a881054c2070cf4b42279e7dcd6aef7326aa85f
mia0222 <145412613+mia0222@users.noreply.github.com>
2023-10-25 11:40:49 +0900

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -1,115 +1,55 @@
-/**
- * @license
- * Copyright 2023 NPCshim All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
-/**
- * Google CodeLab "Make a smart webcam in JavaScript with a TensorFlow.js pre-trained Machine Learning model" を基に作成しました。
- * This code is based on "Make a smart webcam in JavaScript with a TensorFlow.js pre-trained Machine Learning model."
- * URL: https://codelabs.developers.google.com/codelabs/tensorflowjs-object-detection
- * 元コードの著作権は Google LLC. と Jason Mayes氏に帰属します。
- * The original code is copyrighted by Google LLC. and Jason Mayes.
- */
+const startCameraButton = document.getElementById('start-camera-button');
+const cameraFeed = document.getElementById('camera-feed');
+const outputCanvas = document.getElementById('output');
 
-const video = document.getElementById('webcam');  // Stream.
-const liveView = document.getElementById('liveView');  // Container.
-const demosSection = document.getElementById('demos');
-const enableWebcamButton = document.getElementById('webcamButton');
+let model;
+let children = [];
 
-// Check if webcam access is supported.
-function getUserMediaSupported() {
-  return !!(navigator.mediaDevices &&
-    navigator.mediaDevices.getUserMedia);
+startCameraButton.addEventListener('click', initializeCamera);
+
+async function initializeCamera() {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    cameraFeed.srcObject = stream;
+    startCameraButton.style.display = 'none';
+    cameraFeed.style.display = 'block';
+
+    model = await cocoSsd.load();
+    detectFrame();
 }
 
-// If webcam supported, add event listener to button for when user
-// wants to activate it to call enableCam function which we will 
-// define in the next step.
-if (getUserMediaSupported()) {
-  enableWebcamButton.addEventListener('click', enableCam);
-} else {
-  console.warn('getUserMedia() is not supported by your browser');
+function detectFrame() {
+    model.detect(cameraFeed).then(predictions => {
+        clearCanvas();
+        for (let i = 0; i < predictions.length; i++) {
+            drawBoundingBox(predictions[i]);
+        }
+        requestAnimationFrame(detectFrame);
+    });
 }
 
-// Enable the live webcam view and start classification.
-function enableCam(event) {
-  // Only continue if the COCO-SSD has finished loading.
-  if (!model) { return; }
-  // Hide the button once clicked.
-  event.target.classList.add('removed');  
-  // getUsermedia parameters to force video but not audio.
-  const constraints = {
-    video: true
-  };
-
-  // Activate the webcam stream.
-  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-    video.srcObject = stream;
-    video.addEventListener('loadeddata', predictWebcam);
-  });
+function drawBoundingBox(prediction) {
+    const ctx = outputCanvas.getContext('2d');
+    const [x, y, width, height] = prediction.bbox;
+    ctx.beginPath();
+    ctx.rect(x, y, width, height);
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'red';
+    ctx.fillStyle = 'transparent';
+    ctx.stroke();
+    ctx.font = '18px Arial';
+    ctx.fillStyle = 'red';
+    ctx.fillText(prediction.class, x, y > 10 ? y - 5 : 10);
 }
 
-// Store the resulting model in the global scope of our app.
-var model = undefined;
+function clearCanvas() {
+    const ctx = outputCanvas.getContext('2d');
+    ctx.clearRect(0, 0, outputCanvas.width, outputCanvas.height);
+}
 
-// Before we can use COCO-SSD class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment 
-// to get everything needed to run.
-cocoSsd.load().then(function (loadedModel) {
-  model = loadedModel;
-  // Show demo section now model is ready to use.
-  demosSection.classList.remove('invisible');
+outputCanvas.width = window.innerWidth;
+outputCanvas.height = window.innerHeight;
+
+window.addEventListener('resize', () => {
+    outputCanvas.width = window.innerWidth;
+    outputCanvas.height = window.innerHeight;
 });
-
-var children = [];
-
-function predictWebcam() {
-  // Now let's start classifying a frame in the stream.
-  model.detect(video).then(function (predictions) {
-    // Remove any highlighting we did previous frame.
-    for (let i = 0; i < children.length; i++) {
-      liveView.removeChild(children[i]);
-    }
-    children.splice(0);
-    // Now lets loop through predictions and draw them to the live view if
-    // they have a high confidence score.
-    for (let n = 0; n < predictions.length; n++) {
-      // If we are over 66% sure we are sure we classified it right, draw it!
-      if (predictions[n].score > 0.66) {
-        const p = document.createElement('p');
-        p.innerText = predictions[n].class  + ' - with ' 
-            + Math.round(parseFloat(predictions[n].score) * 100) 
-            + '% confidence.';
-
-        p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
-            + (predictions[n].bbox[1] - 10) + 'px; width: ' 
-            + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
-
-        const highlighter = document.createElement('div');
-        highlighter.setAttribute('class', 'highlighter');
-        highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
-            + predictions[n].bbox[1] + 'px; width: ' 
-            + predictions[n].bbox[2] + 'px; height: '
-            + predictions[n].bbox[3] + 'px;';
-
-        liveView.appendChild(highlighter);
-        liveView.appendChild(p);
-        children.push(highlighter);
-        children.push(p);
-      }
-    }   
-    // Call this function again to keep predicting when the browser is ready.
-    window.requestAnimationFrame(predictWebcam);
-  });
-}
